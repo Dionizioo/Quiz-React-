0edae27ef1ccb325ea62dd2fa1f7cdc1571ffa8d
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-19 15:35:38 -0300

    Update PickCategory.jsx

--- src/components/PickCategory.jsx
+++ src/components/PickCategory.jsx
@@ -1,17 +1,16 @@
 import { useContext } from "react";
-
 import { QuizContext } from "../context/quiz";
-
-//import Category from "../img/category.svg";
-
 import "./PickCategory.css";
+
+import Scream from "../img/scream.svg";
+import Perso from "../img/perso.svg";
+import Cris from "../img/cris.svg";
 
 const PickCategory = () => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   function chooseCategoryAndReorderQuestions(category) {
     dispatch({ type: "START_GAME", payload: category });
-
     dispatch({ type: "REORDER_QUESTIONS" });
   }
 
@@ -19,21 +18,30 @@ const PickCategory = () => {
     <div id="category">
       <h2>Escolha uma categoria</h2>
       <p>As perguntas serão referentes a uma das linguagens abaixo:</p>
-      {quizState.questions.map((question) => (
-        <button
-          onClick={() => chooseCategoryAndReorderQuestions(question.category)}
-          key={question.category}
-        >
-          {question.category}
-        </button>
-      ))}
-
-
+      <div className="category-item">
+        {quizState.questions.map((question, index) => (
+          <div key={question.category} className="category-button">
+            <button
+              onClick={() => chooseCategoryAndReorderQuestions(question.category)}
+            >
+              <img
+                src={
+                  index === 0
+                    ? Scream
+                    : index === 1
+                    ? Perso
+                    : Cris
+                }
+                alt="Categoria do Quiz"
+                className="category-image-left"
+              />
+              {question.category}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default PickCategory;
-
-
-//<img src={Category} alt="Categoria do Quiz" />
