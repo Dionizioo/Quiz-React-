c0da55a16d8f28e7bc196fd4a16dd20eb309ed5a
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-19 12:16:24 -0300

    Update quiz.jsx

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,31 +1,35 @@
 import { createContext, useReducer } from "react";
 import questions from "../data/questions_complete";
 
+// Definição dos estágios do jogo
 const STAGES = ["Start", "Category", "Playing", "End"];
 
+// Estado inicial do quiz
 const initialState = {
-  gameStage: STAGES[0],
-  questions,
-  currentQuestion: 0,
-  answerSelected: false,
-  score: 0,
-  help: false,
-  optionToHide: null,
+  gameStage: STAGES[0], // Define o estágio inicial como "Start"
+  questions, // Armazena as perguntas importadas
+  currentQuestion: 0, // Índice da pergunta atual
+  answerSelected: false, // Flag indicando se uma resposta foi selecionada
+  score: 0, // Pontuação atual do jogador
+  help: false, // Flag indicando se a ajuda está ativada
+  optionToHide: null, // Opção a ser ocultada na pergunta atual
 };
 
 console.log(initialState);
 
+// Função reducer do quiz
 const quizReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_STAGE":
       return {
         ...state,
-        gameStage: STAGES[1],
+        gameStage: STAGES[1], // Altera o estágio do jogo para "Category"
       };
 
     case "START_GAME":
       let quizQuestions = null;
 
+      // Filtra as perguntas com base na categoria fornecida na ação
       state.questions.forEach((question) => {
         if (question.category === action.payload) {
           quizQuestions = question.questions;
@@ -34,18 +38,19 @@ const quizReducer = (state, action) => {
 
       return {
         ...state,
-        questions: quizQuestions,
-        gameStage: STAGES[2],
+        questions: quizQuestions, // Define as perguntas filtradas
+        gameStage: STAGES[2], // Altera o estágio do jogo para "Playing"
       };
 
     case "REORDER_QUESTIONS":
+      // Reordena as perguntas de forma aleatória
       const reorderedQuestions = state.questions.sort(() => {
         return Math.random() - 0.5;
       });
 
       return {
         ...state,
-        questions: reorderedQuestions,
+        questions: reorderedQuestions, // Define as perguntas reordenadas
       };
 
     case "CHANGE_QUESTION": {
@@ -58,17 +63,17 @@ const quizReducer = (state, action) => {
 
       return {
         ...state,
-        currentQuestion: nextQuestion,
-        gameStage: endGame ? STAGES[3] : state.gameStage,
-        answerSelected: false,
-        help: false,
+        currentQuestion: nextQuestion, // Atualiza a pergunta atual
+        gameStage: endGame ? STAGES[3] : state.gameStage, // Altera o estágio do jogo para "End" se não houver próxima pergunta
+        answerSelected: false, // Reinicia a seleção de resposta
+        help: false, // Desativa a ajuda
       };
     }
 
     case "NEW_GAME": {
       console.log(questions);
       console.log(initialState);
-      return initialState;
+      return initialState; // Reinicia o estado do jogo para o estado inicial
     }
 
     case "CHECK_ANSWER": {
@@ -82,15 +87,15 @@ const quizReducer = (state, action) => {
 
       return {
         ...state,
-        score: state.score + correctAnswer,
-        answerSelected: option,
+        score: state.score + correctAnswer, // Atualiza a pontuação com base na resposta correta
+        answerSelected: option, // Marca a resposta selecionada
       };
     }
 
     case "SHOW_TIP": {
       return {
         ...state,
-        help: "tip",
+        help: "tip", // Ativa a ajuda exibindo uma dica
       };
     }
 
@@ -104,17 +109,18 @@ const quizReducer = (state, action) => {
       let repeat = true;
       let optionToHide;
 
+      // Itera sobre as opções da pergunta atual para encontrar uma opção diferente da resposta correta
       questionWithoutOption.options.forEach((option) => {
         if (option !== questionWithoutOption.answer && repeat) {
-          optionToHide = option;
+          optionToHide = option; // Define a opção a ser ocultada
           repeat = false;
         }
       });
 
       return {
         ...state,
-        optionToHide,
-        help: true,
+        optionToHide, // Define a opção a ser ocultada
+        help: true, // Ativa a ajuda
       };
     }
 
